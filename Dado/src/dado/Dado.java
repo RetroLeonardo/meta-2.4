1fedaa8194e394ab11a75652db8d008fa573886f
Arnulfo <arnulfo.vasquez@uabc.edu.mx>
2021-04-13 11:12:02 -0700

    Moneda

--- Dado/src/dado/Dado.java
+++ Dado/src/dado/Dado.java
@@ -5,5 +5,6 @@ public class Dado {
  public static void main(String[] args) {
      CrearDado dadoPrincipal = new CrearDado();  
 System.out.println("lado: "+ dadoPrincipal.lanzarDado());
+System.out.println("prueba");
  }
 }
